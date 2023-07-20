e300791b99b4f27dad1952a8e97e8ac43f13b364
Bafbi <bafbi3@gmail.com>
2023-07-20 10:37:27 +0200

    add drizzle adapter + create primary table

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,10 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import AzureAd from "@auth/core/providers/azure-ad";
 import { AZUREAD_CLIENT_ID, AZUREAD_CLIENT_SECRET, AZUREAD_TENANT_ID } from "$env/static/private";
+import { SQLiteDrizzleAdapter } from "$lib/server/sqlite-nextauth-adapter";
+import { db } from "$lib/server/db";
 
 export const handle = SvelteKitAuth({
+  adapter: SQLiteDrizzleAdapter(db),
   providers: [AzureAd({ clientId: AZUREAD_CLIENT_ID, clientSecret: AZUREAD_CLIENT_SECRET, tenantId: AZUREAD_TENANT_ID })],
 });
--- src/lib/server/schema.ts
+++ src/lib/server/schema.ts
@@ -1,9 +1,39 @@
-import { sql } from "drizzle-orm";
-import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { sql } from 'drizzle-orm';
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { users, accounts, sessions, verificationTokens } from './sqlite-nextauth-adapter';
 
-export const users = sqliteTable('users', {
-	id: integer('id').primaryKey(),
-	name: text('name').notNull(),
-	email: text('email').notNull(),
+export { users, accounts, sessions, verificationTokens };
+
+export const tickets = sqliteTable('tickets', {
+	id: text('id').notNull().primaryKey(),
+	title: text('title'),
 	createdAt: integer('created_at', { mode: 'timestamp' }).default(sql`(strftime('%s', 'now'))`),
+	updatedAt: integer('updated_at', { mode: 'timestamp' }).default(sql`(strftime('%s', 'now'))`),
+	createdBy: text('created_by')
+		.notNull()
+		.references(() => users.id, { onDelete: 'cascade' }),
+	updatedBy: text('updated_by')
+		.notNull()
+		.references(() => users.id, { onDelete: 'cascade' }),
+	requester: text('requester')
+		.notNull()
+		.references(() => users.id, { onDelete: 'cascade' }),
+	fromService: text('from_service', { enum: ['RH', 'IT'] }).notNull()
 });
+
+export const labels = sqliteTable('labels', {
+	id: text('id').notNull().primaryKey(),
+	name: text('name').notNull().unique(),
+	description: text('description'),
+	color: text('color').notNull(),
+	createdAt: integer('created_at', { mode: 'timestamp' }).default(sql`(strftime('%s', 'now'))`),
+	updatedAt: integer('updated_at', { mode: 'timestamp' }).default(sql`(strftime('%s', 'now'))`),
+	createdBy: text('created_by')
+		.notNull()
+		.references(() => users.id, { onDelete: 'cascade' }),
+	updatedBy: text('updated_by')
+		.notNull()
+		.references(() => users.id, { onDelete: 'cascade' })
+});
+
+
